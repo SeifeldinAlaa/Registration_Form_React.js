7450cca23a041d75f739803fd549678de0b6a90b
Seif Alaa <seifeldin.alaa@gmail.com>
2022-01-11 13:33:11 +0200

    Registration Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="parent-div">
+
+      <div className="form">
+        <div className="ca">Create Account</div>
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email:</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password:</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputCountry1" class="form-label">Country:</label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected></option>
+              <option value="1">Egypt</option>
+              <option value="2">USA</option>
+              <option value="3">Germany</option>
+            </select>
+
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+            <label className="label" class="form-check-label" for="exampleCheck1">I accept the terms of service</label>
+          </div>
+          <button type="submit" >Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
